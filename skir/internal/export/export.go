fbed94242054ba4b8b6eee0089af9b9a5efa567a
Spencer <foxtrotalphalima@gmail.com>
2018-07-21 16:08:08 -0400

    Updated version number

--- skir/internal/export/export.go
+++ skir/internal/export/export.go
@@ -77,7 +77,7 @@ func DataSet(name, query string) error {
 	for _, card := range cards {
 		dat = append(dat, card.CSV(false)...)
 	}
-	path := filepath.Join(skirmish.Cfg.DB.Dir, fmt.Sprintf("%s.csv", name))
+	path := filepath.Join(os.Getenv("GOPATH"), "src", "github.com", "sbrow", "skirmish", fmt.Sprintf("%s.csv", name))
 	f, err := os.Create(path)
 	if err != nil {
 		return err
--- skir/internal/version/version.go
+++ skir/internal/version/version.go
@@ -18,7 +18,7 @@ var CmdVersion = &base.Command{
 	Long:      `Version prints the skir version.`,
 }
 
-const Version = "v0.13.0-14-g5cbc3e9"
+const Version = "v0.13.1"
 
 func runVersion(cmd *base.Command, args []string) {
 	if len(args) != 0 {
